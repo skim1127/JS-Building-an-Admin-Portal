0d93f480f8f968a744315a441094c095136054a5
Jin mo Kim <skim112790@yahoo.com>
2022-01-16 13:46:10 -0500

    Added admin page functionality

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,44 @@
+async function main() {
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+    console.log(books)
 
-// Your Code Here
+    books.forEach(displayBook)
+}
+
+function displayBook(book) {
+    let container = document.querySelector('#root')
+
+    let listContainer = document.createElement('ul')
+
+    let bookContainer = document.createElement('li')
+
+    let bookQuant = document.createElement('input')
+    bookQuant.value = book.quantity
+
+    let saveBtn = document.createElement('button')
+    saveBtn.innerHTML = 'Save'
+
+    saveBtn.addEventListener('click', function() {
+        console.log('Clicked!')
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: bookQuant.value
+            })
+        })
+    })
+
+    bookContainer.append(bookQuant, saveBtn)
+
+    listContainer.append(bookContainer)
+
+    container.append(listContainer)
+}
+
+
+main()
